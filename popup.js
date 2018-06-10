98a0fc4fe3b8aa2858c069d8251570c63f5a9824
anoopsukumaran <anoop.s.achari@gmail.com>
2018-06-10 14:13:30 -0500

    fixed api change

--- popup.js
+++ popup.js
@@ -1,21 +1,47 @@
-
-function convertFormat(dateStr) {
-    var parts = dateStr.split("-");
-    return parts[2] + '-' + parts[1] + '-' + parts[0];
+function formatParser(convertedTs) {
+	var resultMap = convertedTs.split(" ");
+	return resultMap[0];
 }
 
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
     var response = JSON.parse(xmlHttp.responseText);
-	document.getElementById('status').innerHTML = "₹ " + response.rates.INR;
-	var convertedRate = convertFormat(response.date);
-    document.getElementById('date').innerHTML = convertedRate.toString();
+	document.getElementById('status').innerHTML = "₹ " + response.quotes.USDINR.toFixed(2);
+	var convertedTs = convertTs(response.timestamp);
+	var formattedTs = formatParser(convertedTs);
+    document.getElementById('date').innerHTML = formattedTs.toString();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	var url = "http://api.fixer.io/latest?base=USD";
+	var url = "http://apilayer.net/api/live?access_key=8c9fa78c5184e754ace0fb38fd22af7d&format=1";
     httpGet(url); 
 });
+
+function convertTs(Ts) {
+ // Unixtimestamp
+ var unixtimestamp = Ts;
+ // Months array
+ var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+ // Convert timestamp to milliseconds
+ var date = new Date(unixtimestamp*1000);
+ // Year
+ var year = date.getFullYear();
+ // Month
+ var month = months_arr[date.getMonth()];
+ // Day
+ var day = date.getDate();
+ // Hours
+ var hours = date.getHours();
+ // Minutes
+ var minutes = "0" + date.getMinutes();
+ // Seconds
+ var seconds = "0" + date.getSeconds();
+
+ // Display date time in MM-dd-yyyy h:m:s format
+ var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+ 
+ return convdataTime;
+
+}
